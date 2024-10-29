11bd93f0dc657eb0f513258e33f2ae312cefb628
Jaycode01 <josephlamidi01@gmail.com>
2024-10-29 02:35:43 +0100

    Initial commit

---
+++ script.js
@@ -0,0 +1,24 @@
+//Lets go...
+
+const textBox = document.getElementById('textBox');
+const toFahrenheit = document.getElementById('toFahrenheit');
+const toCelsius = document.getElementById('toCelsius');
+const result = document.getElementById('result');
+
+let temp;
+
+function convert(){
+    
+    if (toFahrenheit.checked) {
+        temp = Number(textBox.value);
+        temp = temp * 9/5 + 32;
+        result.textContent = temp.toFixed(1) + '℉';    } 
+    else if(toCelsius.checked){
+        temp = Number(textBox.value);
+        temp = (temp - 32) * 5/9;
+        result.textContent = temp.toFixed(1) + '℃';
+    }
+    else {
+        result.textContent = `Comrade, you need to select a unit 👀`
+    }
+}
